e8f0fac5ef4560b9f7102d9606b4e2139e7461d3
Phan Anh <ptanhvm.ios@gmail.com>
2020-04-06 17:42:29 +0700

    Update README.md, add new Card Layout

--- lib/main.dart
+++ lib/main.dart
@@ -34,9 +34,68 @@ class _MyHomePageState extends State<MyHomePage> {
       ),
       body: Center(
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[],
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: <Widget>[myLayoutWidget()],
         ),
+      ),
+    );
+  }
+
+  Widget myLayoutWidget() {
+    return Container(
+      margin: EdgeInsets.all(16.0),
+      padding: EdgeInsets.all(16.0),
+      decoration: BoxDecoration(
+        color: Colors.purple[900],
+        border: Border.all(),
+        borderRadius: BorderRadius.all(
+          Radius.circular(3.0),
+        ),
+      ),
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: <Widget>[
+          Row(
+            children: [
+              Padding(
+                padding: EdgeInsets.only(right: 8.0),
+                child: Icon(Icons.favorite, color: Colors.green),
+              ),
+              Text(
+                'BEAMS',
+                style: TextStyle(
+                  color: Colors.white,
+                ),
+              ),
+            ],
+          ),
+          Padding(
+            padding: EdgeInsets.symmetric(vertical: 16.0, horizontal: 0),
+            child: Text(
+              'Send programmable push notifications to iOS and Android devices',
+              style: TextStyle(
+                color: Colors.white,
+              ),
+            ),
+          ),
+          Row(
+            children: <Widget>[
+              Text(
+                'EXPLORE BEAMS',
+                style: TextStyle(
+                  color: Colors.green,
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.only(left: 8.0),
+                child: Icon(
+                  Icons.arrow_forward,
+                  color: Colors.green,
+                ),
+              ),
+            ],
+          ),
+        ],
       ),
     );
   }
